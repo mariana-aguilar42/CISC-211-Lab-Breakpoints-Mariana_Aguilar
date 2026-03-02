e1372adbe531b01eccca6170ee34bcd18d7b6960
marianaa42 <mariana05aguilar@gmail.com>
2026-03-02 13:30:17 -0800

    Update asmFunc.s

--- Lab-breakpoints-starter/firmware/src/asmFunc.s
+++ Lab-breakpoints-starter/firmware/src/asmFunc.s
@@ -9,7 +9,7 @@
 /* create a string */
     
 /*** STUDENTS: HINT: this is the string that the C code prints out!  **/
-nameStr: .asciz "Hello. My name is Inigo Montoya."  
+nameStr: .asciz "Hello. My name is Inigo Montoya"  
  
 /* initialize a global variable that C can access to print the nameStr */
 .global nameStrPtr
@@ -67,7 +67,7 @@ asmFunc:
     rsbs r2,r1,0
 
      /* Start of Breakpoint Lab Quiz Section */
-    subs r0,r0,r0  /* STUDENTS! Set a breakpoint at this line! */
+    subs r0,r0,r0  /* Mariana! Set a breakpoint at this line! */
     subs r2,r2,r2
     adds r1, r0, 5
     rsbs r2,r1,0
